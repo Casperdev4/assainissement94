423d16cd8cc32d6d21b59efe59d9a867fa42e57c
Casperdev4 <allam.bilal91@gmail.com>
2024-09-24 14:18:32 +0200

    ddd

--- traitement.php
+++ traitement.php
@@ -74,7 +74,7 @@ try {
 
     $mail->send();
 
-    header('Location: index.html');
+    header('Location: https://assainissement94.fr/');
     exit();
 } catch (Exception $e) {
     echo "Message non envoyé. Erreur Mailer: {$mail->ErrorInfo}";
